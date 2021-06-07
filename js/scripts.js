880d1160484e8052821c067e761353b7be6e021e
Lucian <lucian.miller185@gmail.com>
2021-06-07 09:54:05 -0700

    It will add variable to store user inputed data

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
   $("form#input").submit(function(event) {
     event.preventDefault();
+    const userNumber = parseInt($("#number").val());
+    console.log(userNumber);
   });
 });
