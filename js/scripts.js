6610db702a201160be42194051f4a7a927e6be66
Lucian <lucian.miller185@gmail.com>
2021-06-08 11:56:58 -0700

    It will seperate buisness logic and user logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,19 @@
+//Business Logic------------
+function convertNumber(num) {
+  const numList = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  const romanList = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+  let outputArray = [];
+  if (num > 3999 || num <= 0) return "Please enter a number greater then 0 and less then 3,999";
+};
+
+//User Logic---------------
 $(document).ready(function() {
   $("form#input").submit(function(event) {
     event.preventDefault();
     const userNumber = parseInt($("#number").val());
+    const convertedNumber = (convertNumber(userNumber));
+    console.log(convertedNumber);
     console.log(userNumber);
-
-    const numList = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-    const romanList = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+    $("#roman").text(convertedNumber);
   });
 });
