c5dcf361db1a5a68f24c077a14b8d17aebd24f50
Lucian <lucian.miller185@gmail.com>
2021-06-08 10:47:19 -0700

    It will add array's for numbers and roman numerals

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,8 @@ $(document).ready(function() {
     event.preventDefault();
     const userNumber = parseInt($("#number").val());
     console.log(userNumber);
+
+    const numList = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    const romanList = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
   });
 });
